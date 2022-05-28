03320baf168dc4680ca0321db302e326dfe34336
Johann <johannmrodriguez@gmail.com>
2022-05-28 03:46:23 -0300

    subi ej.1 clase 05

--- TPS/Clase 05/ej1.go
+++ TPS/Clase 05/ej1.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func multiplicar(num1 float32, num2 float32) float32 {
+	return num1 * num2
+}
+
+func main() {
+	var num1 float32 = 9
+	var num2 float32 = 5
+
+	resultadoMultiplicacion := multiplicar(num1, num2)
+	fmt.Println("El resultado de", num1, "x", num2, "es", resultadoMultiplicacion)
 }
