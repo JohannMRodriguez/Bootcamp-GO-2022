6421045edf3a03544b7acce2850d032db34f1c19
Johann <johannmrodriguez@gmail.com>
2022-05-28 03:33:30 -0300

    corregi errores en clase 2 y cree clase 05

--- TPS/Clase 02/ej3.go
+++ TPS/Clase 02/ej3.go
@@ -1,13 +1,12 @@
 package main
-import "fmt"
 
-func main(){
+func main() {
 
-	var lnombre string					//bien definido
-	var apellido string					//bien definido	
-	var int edad						//mal definido, sintaxis incorrecta, correccion: var edad int
-	lapellido := 6						//bien definido, seria igual que: var lapellido int -> lapellido = 6
-	var licencia_de_conducir = true		//bien definido pero no sigue las recomendaciones, debe usar camelCase o PascalCase aca usa snake_case, correccion: licenciaDeConducir
-	var estatura de la persona int		//mal definido, sintaxis incorrecta, correccion: estaturaDeLaPersona
-	cantidadDeHijos := 2				//bien definido
+	// var lnombre string					//bien definido
+	// var apellido string					//bien definido
+	// var int edad							//mal definido, sintaxis incorrecta, correccion: var edad int
+	// lapellido := 6						//bien definido, seria igual que: var lapellido int -> lapellido = 6
+	// var licencia_de_conducir = true		//bien definido pero no sigue las recomendaciones, debe usar camelCase o PascalCase aca usa snake_case, correccion: licenciaDeConducir
+	// var estatura de la persona int		//mal definido, sintaxis incorrecta, correccion: estaturaDeLaPersona
+	// cantidadDeHijos := 2					//bien definido
 }
