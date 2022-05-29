2eff7865d68a465151c399ecf8e1484222011491
Johann <johannmrodriguez@gmail.com>
2022-05-29 07:47:59 -0300

    ej1 clase 6 interfaces

---
+++ TPS/Clase 06 - Interfaces Errors/ej1.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	area() float32
+}
+
+type rectangle struct {
+	width, height float32
+}
+
+type circle struct {
+	radius float32
+}
+
+func (r rectangle) area() float32 {
+	return r.height * r.width
+}
+
+func (c *circle) area() float32 {
+	return c.radius * c.radius * 3.14
+}
+
+func main() {
+
+	rect1 := rectangle{3, 5}
+	circle1 := circle{3.25}
+
+	// fmt.Printf("Area del rectangulo %.2f", rect1.area())
+	// fmt.Println()
+	// fmt.Printf("Area del circulo %.2f", circle1.area())
+
+	shapes := []shape{rect1, &circle1}
+	fmt.Printf("Area del rectangulo %.2f", shapes[0].area())
+	fmt.Println()
+	fmt.Printf("Area del circulo %.2f", shapes[1].area())
+
+}
